b0beceeb76b801fe0d488fb2868a92a0e7a1585d
Chris Grimble <chris@cg-it-solutions.com>
2018-08-01 13:37:06 +0100

    Remove vault settings

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,3 @@
-provider "vault" {
-  address = "https://vault.reform.hmcts.net:6200"
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
   location = "${var.location}"
@@ -10,44 +6,12 @@ resource "azurerm_resource_group" "rg" {
     )}"
 }
 
-data "vault_generic_secret" "email_mac_secret" {
-  path = "secret/${var.infrastructure_env}/sscs/sscs_email_mac_secret_text"
-}
-
-data "vault_generic_secret" "sscs_cor_backend_secret" {
-  path = "secret/${var.infrastructure_env}/ccidam/service-auth-provider/api/microservice-keys/sscs-cor_backend"
-}
-
-data "vault_generic_secret" "idam_api" {
-  path = "secret/${var.infrastructure_env}/sscs/idam_api"
-}
-
-data "vault_generic_secret" "idam_s2s_api" {
-  path = "secret/${var.infrastructure_env}/sscs/idam_s2s_api"
-}
-
-data "vault_generic_secret" "idam_uid" {
-  path = "secret/${var.infrastructure_env}/sscs/idam_uid"
-}
-
-data "vault_generic_secret" "idam_key_sscs" {
-  path = "secret/${var.infrastructure_env}/sscs/idam_key_sscs"
-}
-
-data "vault_generic_secret" "idam_role" {
-  path = "secret/${var.infrastructure_env}/sscs/idam_role"
-}
-
 locals {
   aseName       = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   app_full_name = "${var.product}-${var.component}"
 
   local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
   local_ase = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "core-compute-aat" : "core-compute-saat" : local.aseName}"
-
-  previewVaultName    = "${var.product}"
-  nonPreviewVaultName = "${var.product}-${var.env}"
-  vaultName           = "${(var.env == "preview") ? local.previewVaultName : local.nonPreviewVaultName}"
 }
 
 module "sscs-cor-backend" {
@@ -62,21 +26,6 @@ module "sscs-cor-backend" {
   common_tags  = "${var.common_tags}"
 
   app_settings = {
-    IDAM_API_URL = "${data.vault_generic_secret.idam_api.data["value"]}"
-
-    SUBSCRIPTIONS_MAC_SECRET = "${data.vault_generic_secret.email_mac_secret.data["value"]}"
 
   }
 }
-
-module "sscs-cor-backend-key-vault" {
-  source                  = "git@github.com:hmcts/moj-module-key-vault?ref=master"
-  name                    = "${local.vaultName}"
-  product                 = "${var.product}"
-  env                     = "${var.env}"
-  tenant_id               = "${var.tenant_id}"
-  object_id               = "${var.jenkins_AAD_objectId}"
-  resource_group_name     = "${azurerm_resource_group.rg.name}"
-  product_group_object_id = "300e771f-856c-45cc-b899-40d78281e9c1"
-}
-
