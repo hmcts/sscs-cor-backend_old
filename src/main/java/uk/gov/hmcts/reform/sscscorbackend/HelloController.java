feb223d3208117ca6c0c7efeb14c81bfee3c9194
Chris Grimble <chris@cg-it-solutions.com>
2018-08-01 15:50:31 +0100

    Testing a branch

--- src/main/java/uk/gov/hmcts/reform/sscscorbackend/HelloController.java
+++ src/main/java/uk/gov/hmcts/reform/sscscorbackend/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping(value = "/hello", method = GET)
     public String helloActuator() {
-        return "Hello Spring Boot";
+        return "Hello Spring Boot branch";
     }
 }
