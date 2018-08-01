3a307f27fc42ee7444de9db037594bd8d6cc1e61
Chris Grimble <chris@cg-it-solutions.com>
2018-08-01 13:42:12 +0100

    Remove vault settings

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,11 +1,3 @@
-output "vaultUri" {
-  value = "${module.sscs-cor-backend-key-vault.key_vault_uri}"
-}
-
-output "vaultName" {
-  value = "${local.vaultName}"
-}
-
 output "sscs-output" {
   value = "sscs-output"
 }
