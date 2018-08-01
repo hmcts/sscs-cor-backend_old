e4b49d2a38496b4b2b699ba94920b50b15632378
Chris Grimble <chris@cg-it-solutions.com>
2018-08-01 14:28:16 +0100

    Add hello endpoint and set /health endpoint

---
+++ src/main/java/uk/gov/hmcts/reform/sscscorbackend/HelloController.java
@@ -0,0 +1,15 @@
+package uk.gov.hmcts.reform.sscscorbackend;
+
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello", method= GET)
+    public String helloActuator() {
+        return "Hello Spring Boot";
+    }
+}
