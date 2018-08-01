e34375f88a0595dbac6bbaddc1b98f60bd104114
Chris Grimble <chris@cg-it-solutions.com>
2018-08-01 14:34:35 +0100

    Get build working

--- src/main/java/uk/gov/hmcts/reform/sscscorbackend/HelloController.java
+++ src/main/java/uk/gov/hmcts/reform/sscscorbackend/HelloController.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.sscscorbackend;
 
-
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/hello", method= GET)
+    @RequestMapping(value = "/hello", method = GET)
     public String helloActuator() {
         return "Hello Spring Boot";
     }
